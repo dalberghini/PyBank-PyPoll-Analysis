0b3e5f366580938f9289a6081dc28cb3807dd758
David Alberghini <david.alberghini@gmail.com>
2021-01-06 17:34:59 -0700

    Commented final verison of PyPoll script, results text file also uploaded based off of script

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,23 +1,31 @@
+# Using a csv file and Windows OS, so modules imported
 import os
 import csv
+#Creation of lists and variables for first iteration of code
 voter_id = []
 candidate = []
+#Line 26 uncommented to determine number of candidates, then matching number of vote counters created
 counter_can0= 0
 counter_can1 = 0
 counter_can2 = 0
 counter_can3 = 0
 votes = []
 percent = []
+#Lists created for clean csv viewing
 total_csv = ["Total Votes: "]
 winner_csv = ["Winner: "]
+#importing csv file from local repo and storing header
 input_file = os.path.join("Resources","election_data.csv")
 with open(input_file, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csvheader = next(csvreader)
+#Searching through csv file to determine number of total voters and number of candidates
     for row in csvreader:
         voter_id.append(row[0])
         if row[2] not in candidate:
             candidate.append(row[2])
+#print(candidate)
+#Four candidates found and vote counters added for each candidate, update vote counters based on number of candidates if used for a different election
         if row[2] == candidate[0]:
             counter_can0 = 1 + counter_can0
         elif row[2] == candidate[1]:
@@ -26,12 +34,14 @@ with open(input_file, newline='') as csvfile:
             counter_can2 = 1 + counter_can2
         elif row[2] == candidate[3]:
             counter_can3= 1 + counter_can3
+#Total votes found and Votes list created based on csv file
 total = len(voter_id)
 total_csv.append(total)
 votes.append(counter_can0)
 votes.append(counter_can1)
 votes.append(counter_can2)
 votes.append(counter_can3)
+#Vote percentage formatted, list created, and comparison of results to determine winner
 for x in votes:
     percentage = (x/total)
     percentage  = "{:.3%}".format(percentage)
@@ -45,7 +55,9 @@ if percent[2] > percent[0] and percent[2] > percent[1] and percent[2] > percent[
 if percent[3] > percent[0] and percent[3] > percent[1] and percent[3] > percent[2]:
     winner = candidate[3]
 winner_csv.append(winner)
+#Candidates, percent of votes, and votes for each candidate zipped together
 results_csv = zip(candidate, percent, votes)
+#Election_results function defined, not entirely neccessary, but code looks cleaner with defintion indentation
 def Election_results():
     print("Election Results")
     print("-------------------------")
@@ -58,7 +70,9 @@ def Election_results():
     print("-------------------------")
     print(f'Winner: {winner}')
     print("-------------------------")
+#Results of election printed to terminal
 Election_results()
+#Output csv file created
 output_file = os.path.join("Analysis", "results.csv")
 with open(output_file, "w", newline='') as datafile:
     writer = csv.writer(datafile)
